fd241d0d3f6a266635cb4fee204abb38a21ce759
hikaru-y <h.yamada.bg@gmail.com>
2020-05-04 11:42:18 +0900

    整理

--- Tweet_Sentiment_Extraction/src/train.py
+++ Tweet_Sentiment_Extraction/src/train.py
@@ -2,10 +2,10 @@
 
 
 def get_jaccard_sim(str1, str2):
-    a = set(str1.split())
-    b = set(str2.split())
-    c = a.intersection(b)
-    return float(len(c)) / (len(a) + len(b) - len(c))
+    str1_set = set(str1.split())
+    str2_set = set(str2.split())
+    co_occurrence = str1_set.intersection(str2_set)
+    return float(len(co_occurrence)) / (len(str1_set) + len(str2_set) - len(co_occurrence))
 
 if __name__ == '__main__':
     str1 = 'AI is our friend and it has been friendly'
