f47bc5081df1eeccbdbfc3aecd0ec40326c19915
hikaru-y <h.yamada.bg@gmail.com>
2020-05-04 19:17:42 +0900

    to functions

--- Tweet_Sentiment_Extraction/src/dataloader.py
+++ Tweet_Sentiment_Extraction/src/dataloader.py
@@ -29,53 +29,58 @@ def tokenizer_with_preprocessing(text):
 # test tokenizer with preprocessing
 # print(tokenizer_with_preprocessing('I like dogs.'))
 
+def get_tweets_and_sentiment_label_loaders():
+    #  データを読み込む際の読み込んだ内容に対して行う処理を定義
+    max_length = 256
+    ID = torchtext.data.Field(sequential=False, use_vocab=False)
+    TEXT1 = torchtext.data.Field(sequential=True, tokenize=tokenizer_with_preprocessing, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token="<cls>", eos_token="<eos>") # raw text
+    TEXT2 = torchtext.data.Field(sequential=True, tokenize=tokenizer_with_preprocessing, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token="<cls>", eos_token="<eos>") # selected_text
+    LABEL = torchtext.data.Field(sequential=False, use_vocab=False, preprocessing=lambda l: 0 if l == 'neutral' else 1 if l == 'positive' else 2, is_target=True) # sentiment label
 
-#  データを読み込む際の読み込んだ内容に対して行う処理を定義
-max_length = 256
-ID = torchtext.data.Field(sequential=False, use_vocab=False)
-TEXT1 = torchtext.data.Field(sequential=True, tokenize=tokenizer_with_preprocessing, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token="<cls>", eos_token="<eos>") # raw text
-TEXT2 = torchtext.data.Field(sequential=True, tokenize=tokenizer_with_preprocessing, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token="<cls>", eos_token="<eos>") # selected_text
-LABEL = torchtext.data.Field(sequential=False, use_vocab=False, preprocessing=lambda l: 0 if l == 'neutral' else 1 if l == 'positive' else 2, is_target=True) # sentiment label
+    train_val_ds, test_ds = torchtext.data.TabularDataset.splits(
+        path='../data/', train='train.csv',
+        test='test.csv', format='csv',
+        fields=[('ID', None), ('Text1', TEXT1), ('Text2', TEXT2), ('Label', LABEL)],
+        skip_header=True)
 
-train_val_ds, test_ds = torchtext.data.TabularDataset.splits(
-    path='../data/', train='train.csv',
-    test='test.csv', format='csv',
-    fields=[('ID', None), ('Text1', TEXT1), ('Text2', TEXT2), ('Label', LABEL)],
-    skip_header=True)
+    # test dataloader
+    # print('訓練 検証のデータ数: {}'.format(len(train_val_ds)))
+    # print('１つ目の訓練&検証データ:{}'.format(vars(train_val_ds[0])))
 
-# test dataloader
-print('訓練 検証のデータ数: {}'.format(len(train_val_ds)))
-print('１つ目の訓練&検証データ:{}'.format(vars(train_val_ds[0])))
+    train_ds, val_ds = train_val_ds.split(split_ratio=0.8, random_state=random.seed(1234))
+    # # test split data
+    # print(len(train_ds))
+    # print(len(val_ds))
+    # print(vars(train_ds[0]))
 
-train_ds, val_ds = train_val_ds.split(split_ratio=0.8, random_state=random.seed(1234))
-# # test split data
-# print(len(train_ds))
-# print(len(val_ds))
-# print(vars(train_ds[0]))
+    # make vocab
+    fasttext_vectors = Vectors(name='../data/wiki-news-300d-1M.vec')
+    # test vectors
+    # print(fasttext_vectors.dim)
+    # print(len(fasttext_vectors.itos))
 
-# make vocab
-fasttext_vectors = Vectors(name='../data/wiki-news-300d-1M.vec')
-# test vectors
-# print(fasttext_vectors.dim)
-# print(len(fasttext_vectors.itos))
-
-#  ベクトル化したボキャブラリーを作成
-TEXT1.build_vocab(train_ds, vectors=fasttext_vectors, min_freq=10)
-TEXT2.build_vocab(train_ds, vectors=fasttext_vectors, min_freq=10)
-# ボキャブラリのベクトル確認
-# print(TEXT1.vocab.vectors.shape)
-# print(TEXT1.vocab.vectors)
-# print(TEXT1.vocab.stoi)
+    #  ベクトル化したボキャブラリーを作成
+    TEXT1.build_vocab(train_ds, vectors=fasttext_vectors, min_freq=10)
+    TEXT2.build_vocab(train_ds, vectors=fasttext_vectors, min_freq=10)
+    # ボキャブラリのベクトル確認
+    # print(TEXT1.vocab.vectors.shape)
+    # print(TEXT1.vocab.vectors)
+    # print(TEXT1.vocab.stoi)
 
 
-# make Dataloader
-train_dl = torchtext.data.Iterator(train_ds, batch_size=24, train=True)
-val_dl = torchtext.data.Iterator(val_ds, batch_size=24, train=False, sort=False)
-test_dl = torchtext.data.Iterator(test_ds, batch_size=24, sort=False)
+    # make Dataloader
+    train_dl = torchtext.data.Iterator(train_ds, batch_size=24, train=True)
+    val_dl = torchtext.data.Iterator(val_ds, batch_size=24, train=False, sort=False)
+    test_dl = torchtext.data.Iterator(test_ds, batch_size=24, sort=False)
 
-# test
-batch = next(iter(val_dl))
-# print(batch.ID)
-print(batch.Text1)
-print(batch.Text2)
-print(batch.Label)
+    # test
+    # batch = next(iter(train_dl))
+    # print(batch.Text1)
+    # print(batch.Text2)
+    # print(batch.Label)
+
+    return train_dl, val_dl, test_dl, TEXT1, TEXT2
+
+
+if __name__ == '__main__':
+    get_tweets_and_sentiment_label_loaders()
