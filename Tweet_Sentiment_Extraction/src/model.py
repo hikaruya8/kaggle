d858c8abc83f9a08bf498511625a9f3adb6a572b
hikaru-y <h.yamada.bg@gmail.com>
2020-05-05 06:27:13 +0900

    TransformerBlock_function_made

--- Tweet_Sentiment_Extraction/src/model.py
+++ Tweet_Sentiment_Extraction/src/model.py
@@ -1,6 +1,7 @@
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from dataloader import get_tweets_and_sentiment_label_loaders
 
 train_dl, val_dl, test_dl, TEXT1, TEXT2 = get_tweets_and_sentiment_label_loaders()
@@ -56,6 +57,7 @@ class PositionalEncoder(nn.Module):
         ret = math.sqrt(self.d_model)*x + self.pe
         return ret
 
+
 '''
 # test
 # prepare minibatch
@@ -71,4 +73,112 @@ x2 = net2(x1)
 print("input tensor size:{}".format(x1.shape))
 print("output tensor size:{}".format(x2.shape))
 '''
+
+
+
+class Attention(nn.Module):
+    def __init__(self, d_model=300):
+        super().__init__()
+        #全結合層で特徴量を変換
+        self.q_linear = nn.Linear(d_model, d_model)
+        self.k_linear = nn.Linear(d_model, d_model)
+        self.v_linear = nn.Linear(d_model, d_model)
+
+        # 出力時に使用する全結合層
+        self.out = nn.Linear(d_model, d_model)
+
+        # Attentionの大きさ調整の変数
+        self.d_k = d_model
+
+    def forward(self, q, k, v, mask):
+        # 全結合層で特徴量を変換
+        q = self.q_linear(q)
+        k = self.k_linear(k)
+        v = self.v_linear(v)
+
+        # calc Atention, 各値を足し算するだけだと大きくなってしまいすぎるため、root(d_k)でわって調整
+        weights = torch.matmul(q, k.transpose(1, 2)) / math.sqrt(self.d_k)
+
+        mask = mask.unsqueeze(1)
+        weights = weights.masked_fill(mask == 0, -1e9)
+
+        # softmaxで規格化
+        normalized_weights = F.softmax(weights, dim=-1)
+
+        # AttentionをValueと計算
+        output = torch.matmul(normalized_weights, v)
+
+        # 全結合層で特徴量を変換
+        output = self.out(output)
+
+        return output, normalized_weights
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff=1024, dropout=0.1):
+        super().__init__()
+
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        x = self.linear_1(x)
+        x = self.dropout(F.relu(x))
+        x = self.linear_2(x)
+        return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, d_model, dropout=0.1):
+        super().__init__()
+
+        # LayerNormalization層
+        self.norm_1 = nn.LayerNorm(d_model)
+        self.norm_2 = nn.LayerNorm(d_model)
+
+        # Attention層
+        self.attn = Attention(d_model)
+
+        # Attention後全結合層2つ
+        self.ff = FeedForward(d_model)
+
+        # Dropout
+        self.dropout_1 = nn.Dropout(dropout)
+        self.dropout_2 = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        # 正規化とAttention
+        x_normalized = self.norm_1(x)
+        output, normalized_weights = self.attn(x_normalized, x_normalized, x_normalized, mask)
+        x2 = x + self.dropout_1(output)
+
+        # 正規化と全結合層
+        x_normalized2 = self.norm_2(x2)
+        output = x2 + self.dropout_2(self.ff(x_normalized2))
+
+        return output, normalized_weights
+
+'''test'''
+# model 定義
+net1 = Embedder(TEXT1.vocab.vectors)
+net2 = PositionalEncoder(d_model=300, max_seq_len=256)
+net3 = TransformerBlock(d_model=300)
+
+# mask 作成
+batch = next(iter(train_dl))
+x = batch.Text1[0]
+input_pad = 1 # 単語のIDで'<pad>: 1 のため
+input_mask = (x != input_pad)
+print(input_mask[0])
+
+# 入出力
+x1 = net1(x)
+x2 = net2(x1)
+x3, normalized_weights = net3(x2, input_mask) # self-attentionで特徴量変換
+
+print('入力テンソルサイズ:{}:'.format(x2.shape))
+print('出力テンソルサイズ:{}:'.format(x3.shape))
+print('Attentionのサイズ:{}'.format(normalized_weights.shape))
+
+
 
