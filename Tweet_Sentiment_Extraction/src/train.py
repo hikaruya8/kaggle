9c9c2e773cd1245db7c35c9fc1169bdaa400c724
hikaru-y <h.yamada.bg@gmail.com>
2020-05-05 13:08:08 +0900

    network_settei_done

--- Tweet_Sentiment_Extraction/src/train.py
+++ Tweet_Sentiment_Extraction/src/train.py
@@ -1,29 +1,31 @@
-from dataloader import load_data
-import csv
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+from dataloader import get_tweets_and_sentiment_label_loaders
+from model import TransformerClassification
+import torch.nn as nn
+
+# データの読み込み
+train_dl, val_dl, test_dl, TEXT1, TEXT2 = get_tweets_and_sentiment_label_loaders(max_length=256, batch_size=64)
+# 辞書オブジェクトにまとめる
+dataloaders_dict = {"train": train_dl, "val": val_dl}
+
+# compose model
+net = TransformerClassification(text_embedding_vectors=TEXT1.vocab.vectors, d_model=300, max_seq_len=256, output_dim=3)
+
+# ネットワークの初期化を定義
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        # init Linear Layer
+        nn.init.kaiming_normal_(m.weight)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
+
+# train mode
+# TransformerBlockモジュールを初期化実行
+net.net3_1.apply(weights_init)
+net.net3_2.apply(weights_init)
+
+print("ネットワーク設定完了")
 
 
-sample_submission_data_path = '../data/sample_submission.csv'
-train_data_path = '../data/train.csv'
-test_data_path = '../data/test.csv'
-
-def load_data(path):
-    return pd.read_csv(path)
-
-def train_svm():
-    train_data = load_data(train_data_path)
-    test_data = load_data(test_data_path)
-    X = train_data['text']
-    y = train_data['sentiment']
-    import pdb;pdb.set_trace()
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=1,test_size=0.1)
-
-
-
-
-
-
-if __name__ == '__main__':
-    train_svm()
+# if __name__ == '__main__':
+#     train_svm()
