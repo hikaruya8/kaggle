ddbc0c0d540307b10c63994b4fee69ac8cce8461
hikaru-y <h.yamada.bg@gmail.com>
2020-05-06 14:58:59 +0900

    LayerNormalization_done

---
+++ Tweet_Sentiment_Extraction/src/bert_model.py
@@ -0,0 +1,13 @@
+class BertLayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        super(BertLayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(hidden_size)) # =weight
+        self.beta = nn.Parameter(torch.zeros(hidden_size)) # =bias
+        self.variance_epsilon = eps
+
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x-u).pow(2).mean(-1, keepdim=True)
+        x = (x-u)/torch.sqrt(s + self.variance_epsilon)
+        return self.gamma*x self.beta
+
